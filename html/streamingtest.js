d947ed18ba523d9414451cfae03d0befc460a76a
Andreas Fransson <Ankanx@gmail.com>
2018-11-12 19:47:43 +0100

    testing

--- html/streamingtest.js
+++ html/streamingtest.js
@@ -173,7 +173,7 @@ $(document).ready(function() {
 									var addButtons = false;
 									if($('#remotevideo').length === 0) {
 										addButtons = true;
-										$('#stream').append('<video class="rounded centered hide" id="remotevideo" width=320 height=240 autoplay playsinline/>');
+										$('#stream').append('<video class="rounded centered hide" id="remotevideo" width=640 height=480 autoplay playsinline/>');
 										// Show the stream and hide the spinner when we get a playing event
 										$("#remotevideo").bind("playing", function () {
 											$('#waitingvideo').remove();
@@ -326,7 +326,7 @@ function startStream() {
 	var body = { "request": "watch", id: parseInt(selectedStream) };
 	streaming.send({"message": body});
 	// No remote video yet
-	$('#stream').append('<video class="rounded centered" id="waitingvideo" width=320 height=240 />');
+	$('#stream').append('<video class="rounded centered" id="waitingvideo" width=640 height=480 />');
 	if(spinner == null) {
 		var target = document.getElementById('stream');
 		spinner = new Spinner({top:100}).spin(target);
